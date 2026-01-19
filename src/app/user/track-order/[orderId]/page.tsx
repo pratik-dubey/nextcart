7797ce645bc94d6610d360f16a3b4839dad3d927
Pratik Dubey <pratikdubey091@gmail.com>
2026-01-19 12:24:30 +0530

    integrated ai chat feature for both user and delivery boy

--- src/app/user/track-order/[orderId]/page.tsx
+++ src/app/user/track-order/[orderId]/page.tsx
@@ -3,9 +3,9 @@ import LiveMap from '@/components/LiveMap'
 import { getSocket } from '@/lib/socket'
 import { IUser } from '@/models/user.model'
 import axios from 'axios'
-import { ArrowLeft, Send } from 'lucide-react'
+import { ArrowLeft, Loader, Send, Sparkle } from 'lucide-react'
 import { useParams, useRouter } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { AnimatePresence, motion } from 'motion/react'
 import { IMessage } from '@/models/message.model'
 import { useSelector } from 'react-redux'
@@ -50,6 +50,9 @@ interface ILocation {
 function TrackOrder({ params }: { params: { orderId: string } }) {
     const [order, setOrder] = useState<IOrder>()
     const router = useRouter()
+    const [loading, setLoading] = useState(false)
+    const [suggestions, setSuggestions] = useState([])
+    const chatBoxRef = useRef<HTMLDivElement>(null)
     const [userLocation, setUserLocation] = useState<ILocation>(
         {
             latitude: 0,
@@ -140,6 +143,28 @@ function TrackOrder({ params }: { params: { orderId: string } }) {
         }
         getAllMessages()
       }, [])
+
+      useEffect(() => {
+        chatBoxRef.current?.scrollTo({
+            top:chatBoxRef.current.scrollHeight,
+            behavior:"smooth"
+        })
+      }, [messages])
+
+      const getSuggestion=async ()=>{
+        setLoading(true)
+        try {
+          // filtering last message of user
+          const lastMessage=messages?.filter(m=>m.senderId.toString()!==userData?._id)?.at(-1)
+          const result=await axios.post("/api/chat/ai-suggestions",{message:lastMessage?.text,role:"user"})
+          console.log(result.data)
+        setSuggestions(result.data)
+        setLoading(false)
+        } catch (error) {
+          console.log(error)
+          setLoading(false)
+        }
+      }
     return (
         <div className='w-full min-h-screen bg-linear-to-b from-green-50 to-white'>
             <div className='max-w-2xl mx-auto pb-24'>
@@ -160,11 +185,34 @@ function TrackOrder({ params }: { params: { orderId: string } }) {
 
                     <div className='bg-white rounded-3xl shadow-lg border p-4 h-[430px] flex flex-col'>
 
-                        <div className='flex-1 overflow-y-auto p-2 space-y-3'>
+                    <div className='flex justify-between items-center mb-3'>
+        <span className='font-semibold text-gray-700 text-sm'>Quick Replies</span>
+        <motion.button
+          whileTap={{ scale: 0.9 }}
+          disabled={loading}
+          onClick={getSuggestion}
+          className="px-3 py-1 text-xs flex items-center gap-1 bg-purple-100 text-purple-700 rounded-full shadow-sm border border-purple-200 cursor-pointer"
+
+        ><Sparkle size={14} />{loading ? <Loader className="w-5 h-5 animate-spin" /> : "AI suggest"}</motion.button>
+      </div>
+
+      <div className='flex gap-2 flex-wrap mb-3'>
+        {suggestions.map((s, i) => (
+          <motion.div
+            key={s}
+            whileTap={{ scale: 0.92 }}
+            className="px-3 py-1 text-xs bg-green-50 border border-green-200 cursor-pointer text-green-700 rounded-full"
+            onClick={() => setNewMessage(s)}
+          >
+            {s}
+          </motion.div>
+        ))}
+      </div>
+                        <div className='flex-1 overflow-y-auto p-2 space-y-3' ref={chatBoxRef}>
                             <AnimatePresence>
                                 {messages?.map((msg, index) => (
                                     <motion.div
-                                        key={msg._id ? msg._id.toString() : `${msg.senderId}-${msg.time}`}
+                                        key={index}
                                         initial={{ opacity: 0, y: 15 }}
                                         animate={{ opacity: 1, y: 0 }}
                                         exit={{ opacity: 0 }}
--- src/components/DeliveryChat.tsx
+++ src/components/DeliveryChat.tsx
@@ -1,65 +1,113 @@
 import { getSocket } from '@/lib/socket'
 import { IMessage } from '@/models/message.model'
 import axios from 'axios'
-import { Send } from 'lucide-react'
-import {AnimatePresence, motion} from 'motion/react'
-import React, { useEffect, useState } from 'react'
+import { Loader, Send, Sparkle } from 'lucide-react'
+import { AnimatePresence, motion } from 'motion/react'
+import React, { useEffect, useRef, useState } from 'react'
 
 type props = {
-    orderId: string,
-    deliveryBoyId: string
-  }
-  
+  orderId: string,
+  deliveryBoyId: string
+}
+
 function DeliveryChat({ orderId, deliveryBoyId }: props) {
-    const [newMessage, setNewMessage] = useState("")
-    const [messages, setMessages] = useState<IMessage[]>([])
-    useEffect(() => {
-        const socket = getSocket()
-        socket.emit("join-room", orderId)
-        socket.on("send-message", (message) => {
-            setMessages((prev) => [...prev!,message])
-        })
-        return () => {
-            socket.off("send-message")
-          }
-    }, [])
-
-    const sendMsg = () => {
-        const socket = getSocket()
-        const message = {
-            roomId:orderId,
-            text:newMessage,
-            senderId:deliveryBoyId,
-            time:new Date().toLocaleTimeString([],{
-                hour:"2-digit",
-                minute:"2-digit"
-            })
-        }
-        socket.emit("send-message", message)
-        setNewMessage("")
+  const [loading, setLoading] = useState(false)
+  const [suggestions, setSuggestions] = useState([])
+  const [newMessage, setNewMessage] = useState("")
+  const [messages, setMessages] = useState<IMessage[]>([])
+  const chatBoxRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    const socket = getSocket()
+    socket.emit("join-room", orderId)
+    socket.on("send-message", (message) => {
+      setMessages((prev) => [...prev!, message])
+    })
+    return () => {
+      socket.off("send-message")
     }
+  }, [])
 
-    useEffect(() => {
-        const getAllMessages = async () => {
-          try {
-            const result = await axios.post("/api/chat/messages", { roomId: orderId })
-            // console.log("got all messages")
-            console.log(result.data)
-            setMessages(result.data)
-          } catch (error) {
-            console.log(error)
-          }
-        }
-        getAllMessages()
-      }, [])
+  const sendMsg = () => {
+    const socket = getSocket()
+    const message = {
+      roomId: orderId,
+      text: newMessage,
+      senderId: deliveryBoyId,
+      time: new Date().toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit"
+      })
+    }
+    socket.emit("send-message", message)
+    setNewMessage("")
+  }
+
+  useEffect(() => {
+    const getAllMessages = async () => {
+      try {
+        const result = await axios.post("/api/chat/messages", { roomId: orderId })
+        // console.log("got all messages")
+        console.log(result.data)
+        setMessages(result.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getAllMessages()
+  }, [])
+
+  useEffect(() => {
+    chatBoxRef.current?.scrollTo({
+      top: chatBoxRef.current.scrollHeight,
+      behavior: "smooth"
+    })
+  }, [messages])
+
+  const getSuggestion=async ()=>{
+    setLoading(true)
+    try {
+      // filtering last message of user
+      const lastMessage=messages?.filter(m=>m.senderId.toString()!==deliveryBoyId)?.at(-1)
+      const result=await axios.post("/api/chat/ai-suggestions",{message:lastMessage?.text,role:"delivery_boy"})
+      console.log(result.data)
+    setSuggestions(result.data)
+    setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
   return (
     <div className='bg-white rounded-3xl shadow-lg border p-4 h-[430px] flex flex-col'>
 
-<div className='flex-1 overflow-y-auto p-2 space-y-3'>
+      <div className='flex justify-between items-center mb-3'>
+        <span className='font-semibold text-gray-700 text-sm'>Quick Replies</span>
+        <motion.button
+          whileTap={{ scale: 0.9 }}
+          disabled={loading}
+          onClick={getSuggestion}
+          className="px-3 py-1 text-xs flex items-center gap-1 bg-purple-100 text-purple-700 rounded-full shadow-sm border border-purple-200 cursor-pointer"
+
+        ><Sparkle size={14} />{loading ? <Loader className="w-5 h-5 animate-spin" /> : "AI suggest"}</motion.button>
+      </div>
+
+      <div className='flex gap-2 flex-wrap mb-3'>
+        {suggestions.map((s, i) => (
+          <motion.div
+            key={i}
+            whileTap={{ scale: 0.92 }}
+            className="px-3 py-1 text-xs bg-green-50 border border-green-200 cursor-pointer text-green-700 rounded-full"
+            onClick={() => setNewMessage(s)}
+          >
+            {s}
+          </motion.div>
+        ))}
+      </div>
+      <div className='flex-1 overflow-y-auto p-2 space-y-3' ref={chatBoxRef}>
         <AnimatePresence>
           {messages?.map((msg, index) => (
             <motion.div
-            key={msg._id ? msg._id.toString() : `${msg.senderId}-${msg.time}`}
+              key={msg._id ? msg._id.toString() : `${msg.senderId}-${msg.time}`}
               initial={{ opacity: 0, y: 15 }}
               animate={{ opacity: 1, y: 0 }}
               exit={{ opacity: 0 }}
@@ -80,9 +128,9 @@ function DeliveryChat({ orderId, deliveryBoyId }: props) {
         </AnimatePresence>
       </div>
 
-         <div className='flex gap-2 mt-3 border-t pt-3'>
+      <div className='flex gap-2 mt-3 border-t pt-3'>
         <input type="text" placeholder='Type a Message...' className='flex-1 bg-gray-100 px-4 py-2 rounded-xl outline-none focus:ring-2 focus:ring-green-500' value={newMessage} onChange={(e) => setNewMessage(e.target.value)} />
-        <button onClick={sendMsg}className='bg-green-600 hover:bg-green-700 p-3 rounded-xl text-white' ><Send size={18} /></button>
+        <button onClick={sendMsg} className='bg-green-600 hover:bg-green-700 p-3 rounded-xl text-white' ><Send size={18} /></button>
       </div>
     </div>
   )
