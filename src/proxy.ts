bd46110f1551da17915c6345104af380c92ebbf0
Pratik Dubey <pratikdubey091@gmail.com>
2026-01-29 12:58:18 +0530

    ...

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,56 +1,3 @@
-// import { getToken } from "next-auth/jwt";
-// import { NextRequest, NextResponse } from "next/server";
-
-// export async function proxy(req: NextRequest) {
-//   //     req.url = RAW URL string (full address in one line)
-//   // req.nextUrl = Parsed Next.js URL object (address broken into parts)
-
-//   const { pathname } = req.nextUrl;
-//   //   console.log(pathname);
-//   const publicRoutes = [
-//     "/auth/signin",
-//     "/auth/signup",
-//     "/api/auth",
-//     "/_next",
-//     "/favicon.ico",
-//   ];
-//   //   console.log(req.url);
-//   if (publicRoutes.some((path) => pathname.startsWith(path))) {
-//     return NextResponse.next();
-//   }
-//   const token = await getToken({ req, secret: process.env.AUTH_SECRET });
-//   if (!token) {
-//     //       And when you pass a relative path ("/auth/signin") to new URL(),
-//     // it ALWAYS attaches it to the origin, NOT to the path.
-//     const loginUrl = new URL("/auth/signin", req.url);
-//     loginUrl.searchParams.set("callbackUrl", req.url);
-//     return NextResponse.redirect(loginUrl);
-//   }
-
-//   const role = token.role;
-//   if (pathname.startsWith("/user") && role !== "user") {
-//     return NextResponse.redirect(new URL("/unauthorized", req.url));
-//   }
-//   if (pathname.startsWith("/delivery") && role !== "deliveryBoy") {
-//     return NextResponse.redirect(new URL("/unauthorized", req.url));
-//   }
-//   if (pathname.startsWith("/admin") && role !== "admin") {
-//     return NextResponse.redirect(new URL("/unauthorized", req.url));
-//   }
-//   return NextResponse.next();
-// }
-
-// // The routes you mention inside (?! ... ) are totally ignored by middleware.
-// export const config = {
-//   matcher: "/((?!api/auth|auth|_next/static|_next/image|favicon.ico).*)",
-// };
-
-
-
-
-
-
-
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "./auth";
 
@@ -83,8 +30,12 @@ export async function proxy(req: NextRequest) {
     return NextResponse.next();
   }
 
- const session = await auth()
+//  const session = await auth()
+const hasSessionCookie =
+  req.cookies.has("__Secure-next-auth.session-token") ||
+  req.cookies.has("next-auth.session-token");
 
+const session = hasSessionCookie ? await auth() : null;
   // if (!session) {
   //   const loginUrl = new URL("/auth/signin", req.url);
   //   loginUrl.searchParams.set("callbackUrl", req.url);
